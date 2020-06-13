c7989835b8302379ea1512f3a81e852f11574535
Augusto Sandim <augustosandim@gmail.com>
2020-06-12 23:36:35 -0400

    Test deploy master

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Index = () => {
     dispatch(startClock())
   }, [dispatch])
 
-  return <Page title="VIVA VEG - PRODUCAO" linkTo="/other" NavigateTo="Other Page" />
+  return <Page title="VIVA VEG - PRODUCAO NOVO" linkTo="/other" NavigateTo="Other Page" />
 }
 
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
